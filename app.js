6f66243a86f98b0b879f84aca4773cbcebdc2d64
daniel-logo <daniel-logo>
2024-02-13 14:30:34 +0100

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("myNav").style.width= '100%';
+    document.getElementsByClassName("overlayBackground").style.display= 'block';
+}
+function closeNav() {
+    document.getElementById('myNav').style.width= '0%';
+    document.getElementsByClassName("overlayBackground").style.display= 'none';
+}
